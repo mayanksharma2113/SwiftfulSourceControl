7dda54341b85c618390736cbc0ebaf306c3b8629
Mayank <mayank@soverin.net>
2024-12-20 17:28:30 +0100

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Mayank Sharma on 20.12.24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
