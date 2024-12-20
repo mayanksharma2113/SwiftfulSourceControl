5f658988753eefdcecad43f04b92cfdeda0a4ca0
Mayank <mayank@soverin.net>
2024-12-20 17:40:36 +0100

    [Clean] Add commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,27 @@
 //  Created by Mayank Sharma on 20.12.24.
 //
 
+/*
+ 
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUTION:
+ [Patch] Description of patch
+ 
+ BUT NOT IN PRODUCTION:
+ [BUG] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
