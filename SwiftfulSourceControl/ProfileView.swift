80604e7cd2fd5b7028f25b3566b883b078a014f8
Mayank <mayank@soverin.net>
2024-12-22 20:44:49 +0000

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Mayank Sharma on 22.12.24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Nick!!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
